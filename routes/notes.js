48131ee6b48540e6a35c60b86d57bd15a1e79610
tatakuku9950 <sr20det31spr@hotmail.com>
2024-11-26 19:57:42 +0900

    add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -1,33 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const { MongoClient } = require("mongodb");
 
-const uri = "";
-const client = new MongoClient(uri);
-
-router.get('/', async (req, res) => {
-  try {
-    // クライアント接続を確認
-    await client.connect();
-
-    // データベースとコレクションを指定
-    const database = client.db('notes');
-    const notes = database.collection('notes');
-
-    // クエリ条件を指定
-    const query = { id: 2 };
-    const note = await notes.findOne(query);
-
-    // データをレスポンスとして返す
-    res.json(note);
-  } catch (error) {
-    // エラー処理
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  } finally {
-    // クライアント接続を閉じる
-    await client.close();
-  }
-});
-
-module.exports = router;
+const{ MongoClient} = require("mongodb");
+const uri= "";
+const client= new MongoClient(uri);
+router.get('/', async(req, res) =>{
+// データベース、コレクションを指定
+const database= client.db('notes');
+const notes= database.collection('notes');
+// idが１のドキュメントを取得
+const query= { id: 2 };
+const note= await notes.findOne(query);
+res.json(note);
+})
+module.exports= router;
